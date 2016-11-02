c76b72295f040d0a82286adc84b42f034579f99f
internelp <gaof.d@foxmail.com>
2016-11-02 15:11:09 +0800

    完成

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# golang-zabbix-alter-to-dingding
-zabbix报警到钉钉
+# golang-zabbix-alter-to-wechat
+zabbix报警到微信
 
-更多详情：http://www.qiansw.com/golang-zabbix-alter-to-dingding.html
-
-
-##变更记录
-	2016-08-01 21:22		增加了对xml源消息的支持，防止json格式的消息中有引号造成消息失败的问题。
-
-
-#####未实现的功能
-- log记录到文件
-- log不齐全
-- 禁止要求非空参数的提醒
+更多详情：http://www.qiansw.com/golang-zabbix-alter-to-wechat.html
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 //执行方式：
 //alter -to=接收人 -agent=应用id	-color=消息头部颜色	-corpid=corpid		   -corpsecret=corpsecret
 //alter -to=@all  -agent=29481187 -color=FFE61A1A 	-corpid=dingd123465865 -corpsecret=zC5Jbed9S
-//CorpID和CorpSecret可以在钉钉后台找到
+//CorpID和CorpSecret可以在微信后台找到
 import (
 	"bytes"
 	"encoding/json"
@@ -19,7 +19,8 @@ import (
 
 type MsgInfo struct {
 	//消息属性和内容
-	To, Agentid, Corpid, Corpsecret, Msg, Url, Style string
+	To, Corpid, Corpsecret, Msg, Url, Style string
+	Agentid                                 int
 }
 
 var msgInfo MsgInfo
@@ -41,128 +42,91 @@ type Alter struct {
 	Acknowledgement        string `json:"acknowledgement" xml:"acknowledgement"`
 	Acknowledgementhistory string `json:"acknowledgementhistory" xml:"acknowledgementhistory"`
 }
-type DingMsg struct {
+
+type WechatMsg struct {
 	Touser  string `json:"touser"`
-	Agentid string `json:"agentid"`
+	Toparty string `json:"toparty"`
+	Totag   string `json:"totag"`
 	Msgtype string `json:"msgtype"`
-	Oa      struct {
-		MessageURL string `json:"message_url"`
-		Head       struct {
-			Bgcolor string `json:"bgcolor"`
-		} `json:"head"`
-		Body struct {
-			Title string `json:"title"`
-			Form  [5]struct {
-				Key   string `json:"key"`
-				Value string `json:"value"`
-			} `json:"form"`
-			Rich struct {
-				Num string `json:"num"`
-			} `json:"rich"`
-			Content string `json:"content"`
-			Author  string `json:"author"`
-		} `json:"body"`
-	} `json:"oa"`
+	Agentid int    `json:"agentid"`
+	Text    struct {
+		Content string `json:"content"`
+	} `json:"text"`
+	Safe int `json:"safe"`
 }
 
 func init() {
 	//	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	flag.StringVar(&msgInfo.To, "to", "@all", "消息的接收人，可以在钉钉后台查看，可空。")
-	flag.StringVar(&msgInfo.Agentid, "agentid", "", "AgentID，可以在钉钉后台查看，不可空。")
-	flag.StringVar(&msgInfo.Corpid, "corpid", "", "CorpID，可以在钉钉后台查看，不可空。")
-	flag.StringVar(&msgInfo.Corpsecret, "corpsecret", "", "CorpSecret，可以在钉钉后台查看，不可空。")
-	flag.StringVar(&msgInfo.Msg, "msg", `{ "from": "千思网", "time": "2016.07.28 17:00:05", "level": "Warning", "name": "这是一个千思网（qiansw.com）提供的ZABBIX钉钉报警插件。", "key": "icmpping", "value": "30ms", "now": "56ms", "id": "1637", "ip": "8.8.8.8", "color":"FF4A934A", "age":"3m", "recoveryTime":"2016.07.28 17:03:05", "status":"OK" }`, "Json格式的文本消息内容，不可空。")
-	flag.StringVar(&msgInfo.Url, "url", "http://www.qiansw.com/golang-zabbix-alter-to-dingding.html", "消息内容点击后跳转到的URL，可空。")
+	flag.StringVar(&msgInfo.To, "to", "@all", "消息的接收人，可以在微信后台查看，可空。")
+	flag.IntVar(&msgInfo.Agentid, "agentid", 1, "AgentID，可以在微信后台查看，不可空。")
+	flag.StringVar(&msgInfo.Corpid, "corpid", "", "CorpID，可以在微信后台查看，不可空。")
+	flag.StringVar(&msgInfo.Corpsecret, "corpsecret", "", "CorpSecret，可以在微信后台查看，不可空。")
+	flag.StringVar(&msgInfo.Msg, "msg", `{ "from": "千思网", "time": "2016.07.28 17:00:05", "level": "Warning", "name": "这是一个千思网（qiansw.com）提供的ZABBIX微信报警插件。", "key": "icmpping", "value": "30ms", "now": "56ms", "id": "1637", "ip": "8.8.8.8", "color":"FF4A934A", "age":"3m", "recoveryTime":"2016.07.28 17:03:05", "status":"OK" }`, "Json格式的文本消息内容，不可空。")
+	flag.StringVar(&msgInfo.Url, "url", "http://www.qiansw.com/golang-zabbix-alter-to-wechat.html", "消息内容点击后跳转到的URL，可空。")
 	flag.StringVar(&msgInfo.Style, "style", "json", "Msg的格式，可选json和xml，推荐使用xml（支持消息中含双引号），可空。")
 	flag.Parse()
-	log.Println("[Init] 初始化完成。")
+	log.Println("初始化完成。")
 }
 
 func makeMsg(msg string) string {
 	//	根据json或xml文本创建消息体
-	log.Println("[makeMsg] 开始创建消息。")
+	log.Println("开始创建消息。")
 	var alter Alter
 	if msgInfo.Style == "xml" {
-		log.Println("[makeMsg] 来源消息格式为XML。")
+		log.Println("来源消息格式为XML。")
 		err := xml.Unmarshal([]byte(msg), &alter)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else if msgInfo.Style == "json" {
-		log.Println("[makeMsg] 来源消息格式为Json。")
+		log.Println("来源消息格式为Json。")
 		err := json.Unmarshal([]byte(msg), &alter)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		log.Println("[makeMsg] 未指定来源消息格式，默认使用Json解析。")
+		log.Println("未指定来源消息格式，默认使用Json解析。")
 		err := json.Unmarshal([]byte(msg), &alter)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	log.Printf("[makeMsg] 来源消息为：%s。\r\n", msg)
-	var dingMsg DingMsg
-	//给dingMsg各元素赋值
-	dingMsg.Touser = msgInfo.To
-	dingMsg.Agentid = msgInfo.Agentid
-	dingMsg.Msgtype = "oa"
-	dingMsg.Oa.MessageURL = msgInfo.Url
-	dingMsg.Oa.Head.Bgcolor = alter.Color
-	dingMsg.Oa.Body.Title = alter.Name
-	dingMsg.Oa.Body.Form[0].Key = "告警级别："
-	dingMsg.Oa.Body.Form[1].Key = "故障时间："
-	dingMsg.Oa.Body.Form[2].Key = "故障时长："
-	dingMsg.Oa.Body.Form[3].Key = "IP地址："
-	dingMsg.Oa.Body.Form[4].Key = "检测项："
-	dingMsg.Oa.Body.Form[0].Value = alter.Level
-	dingMsg.Oa.Body.Form[1].Value = alter.Time
-	dingMsg.Oa.Body.Form[2].Value = alter.Age
-	dingMsg.Oa.Body.Form[3].Value = alter.IP
-	dingMsg.Oa.Body.Form[4].Value = alter.Key
-	dingMsg.Oa.Body.Rich.Num = alter.Now
+	log.Printf("来源消息为：%s。\r\n", msg)
+
+	var wechatMsg WechatMsg
+
+	//给wechatMsg各元素赋值
+	wechatMsg.Touser = msgInfo.To
+	wechatMsg.Msgtype = "text"
+	wechatMsg.Agentid = 1
+	wechatMsg.Safe = 0
+	wechatMsg.Text.Content = fmt.Sprintf("%s\n\n级别：%s\n发生：%s\n时长：%s\nIP地址：%s\n检测项：%s\n当前值：%s\n", alter.Name, alter.Level, alter.Time, alter.Age, alter.IP, alter.Key, alter.Now)
+
 	if alter.Status == "PROBLEM" {
 		//  故障处理
-		dingMsg.Oa.Body.Author = fmt.Sprintf("[%s·%s(%s)]", alter.From, "故障", alter.ID)
+		wechatMsg.Text.Content = "[故障]" + wechatMsg.Text.Content
 		if strings.Replace(alter.Acknowledgement, " ", "", -1) == "Yes" {
-			dingMsg.Oa.Body.Content = "故障已经被确认，" + alter.Acknowledgementhistory
+			wechatMsg.Text.Content = wechatMsg.Text.Content + "故障已经被确认，" + alter.Acknowledgementhistory
 		}
+		wechatMsg.Text.Content = wechatMsg.Text.Content + fmt.Sprintf("\n[%s·%s]", alter.From, alter.ID)
+
 	} else if alter.Status == "OK" {
 		//  恢复处理
-		dingMsg.Oa.Body.Form[0].Key = "故障时间："
-		dingMsg.Oa.Body.Form[1].Key = "恢复时间："
-		dingMsg.Oa.Body.Form[0].Value = alter.Time
-		dingMsg.Oa.Body.Form[1].Value = alter.RecoveryTime
-		dingMsg.Oa.Body.Author = fmt.Sprintf("[%s·%s(%s)]", alter.From, "恢复", alter.ID)
+		wechatMsg.Text.Content = fmt.Sprintf("[恢复]%s\n\n级别：%s\n发生：%s\n恢复：%s\n时长：%s\nIP地址：%s\n检测项：%s\n当前值：%s\n", alter.Name, alter.Level, alter.Time, alter.RecoveryTime, alter.Age, alter.IP, alter.Key, alter.Now)
+		wechatMsg.Text.Content = wechatMsg.Text.Content + fmt.Sprintf("\n[%s·%s]", alter.From, alter.ID)
 
-	} else if alter.Status == "msg" {
-		dingMsg.Oa.Body.Title = alter.Name
-		dingMsg.Oa.Body.Form[0].Key = ""
-		dingMsg.Oa.Body.Form[1].Key = ""
-		dingMsg.Oa.Body.Form[2].Key = ""
-		dingMsg.Oa.Body.Form[3].Key = ""
-		dingMsg.Oa.Body.Form[4].Key = ""
-		dingMsg.Oa.Body.Form[0].Value = ""
-		dingMsg.Oa.Body.Form[1].Value = ""
-		dingMsg.Oa.Body.Form[2].Value = ""
-		dingMsg.Oa.Body.Form[3].Value = ""
-		dingMsg.Oa.Body.Form[4].Value = ""
-		dingMsg.Oa.Body.Content = alter.Acknowledgementhistory
 	} else {
 		//  其他status状况处理
-		dingMsg.Oa.MessageURL = "http://www.qiansw.com/golang-zabbix-alter-to-dingding.html"
-		dingMsg.Oa.Body.Content = "ZABBIX动作配置有误，请至千思网[qiansw.com]或直接[点击此消息]查看具体配置文档。"
-		dingMsg.Oa.Body.Author = fmt.Sprintf("[%s·%s(%s)]", alter.From, alter.Status, alter.ID)
-		if strings.Replace(alter.Acknowledgement, " ", "", -1) == "Yes" {
-			dingMsg.Oa.Body.Content = "故障已经被确认，" + alter.Acknowledgementhistory
-		}
+		wechatMsg.Text.Content = wechatMsg.Text.Content + "ZABBIX动作配置有误，请至千思网[http://www.qiansw.com]查看具体配置文档。"
+		wechatMsg.Text.Content = wechatMsg.Text.Content + fmt.Sprintf("\n[%s·%s]", alter.From, alter.ID)
 	}
-	//	创建post给钉钉的Json文本
-	JsonMsg, err := json.Marshal(dingMsg)
+
+	//	创建post给微信的Json文本
+	JsonMsg, err := json.Marshal(wechatMsg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("[makeMsg] 消息创建完成：%s\r\n", string(JsonMsg))
+	log.Printf("消息创建完成：%s\r\n", string(JsonMsg))
 	return string(JsonMsg)
 }
 
@@ -174,7 +138,7 @@ func getToken(corpid, corpsecret string) (token string) { //根据id和secret获
 		Errmsg       string
 	}
 
-	urlstr := fmt.Sprintf("https://oapi.dingtalk.com/gettoken?corpid=%s&corpsecret=%s", corpid, corpsecret)
+	urlstr := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s", corpid, corpsecret)
 	u, _ := url.Parse(urlstr)
 	q := u.Query()
 	u.RawQuery = q.Encode()
@@ -196,6 +160,7 @@ func getToken(corpid, corpsecret string) (token string) { //根据id和secret获
 
 	if err1 == nil {
 		if m.Errcode == 0 {
+			log.Println("AccessToken获取成功。")
 			return m.Access_token
 		} else {
 			log.Fatal("AccessToken获取失败，", m.Errmsg)
@@ -209,9 +174,9 @@ func getToken(corpid, corpsecret string) (token string) { //根据id和secret获
 
 }
 func sendMsg(token, msg string) (status bool) { //发送OA消息，,返回成功或失败
-	log.Printf("[sendMsg] 需要POST的内容：%s\r\n", msg)
+	log.Printf("需要POST的内容：%s\r\n", msg)
 	body := bytes.NewBuffer([]byte(msg))
-	url := fmt.Sprintf("https://oapi.dingtalk.com/message/send?access_token=%s", token)
+	url := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s", token)
 	//	fmt.Println(url)
 	res, err := http.Post(url, "application/json;charset=utf-8", body)
 	if err != nil {
@@ -224,7 +189,7 @@ func sendMsg(token, msg string) (status bool) { //发送OA消息，,返回成功
 		log.Fatal(err)
 		return
 	}
-	log.Printf("[sendMsg] 钉钉接口返回消息：%s\r\n", result)
+	log.Printf("微信接口返回消息：%s\r\n", result)
 
 	return
 }
